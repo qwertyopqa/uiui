26988a90f641ad0925cddb9697dcf357d9ec3508
Goncalo Tavares <goncalo.tavares@pacteraedge.com>
2024-05-08 14:47:45 +0100

    removing dist

--- dist/action/delegate.d.ts
+++
@@ -1,3 +0,0 @@
-export declare const DragDelegate: (tgt: HTMLElement, sets: any) => void;
-export declare const DragOnParentDelegate: (tgt: HTMLElement, sets: any) => void;
-//# sourceMappingURL=delegate.d.ts.map
